eede7e4d5fd18d769ec166567a93d436eb46af6e
rayhong24 <33410377+rayhong24@users.noreply.github.com>
2022-02-21 20:07:17 -0800

    Add continue playing feature
    Removes the need to restart the program if you want to play multiple games in one session.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from class_game import Game
 from class_wordle_bot import Wordle_bot
 
-bob = Wordle_bot()
-
-bob.play()
+while True:
+    print("Starting wordle game...")
+  
+    bot = Wordle_bot
+  
+    bot.play()
+  
+    cont = input("Enter 1 to play again. Enter 0 to end program.")
+  
+    if cont == '0':
+        return 0
+  
+  
+  
